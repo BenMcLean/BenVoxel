8da742527886120f79d198e5c7d8ac85e2eaa647
BenMcLean <mclean.ben@gmail.com>
2024-06-24 12:53:37 -0500

    Leaf doesn't need _init

--- Leaf.cpp
+++ Leaf.cpp
@@ -1,8 +1,4 @@
 #include "Leaf.h"
-void Leaf::_init(Node* parent, std::uint8_t header, std::uint64_t data) {
-	Node::_init(parent, header);
-	this->data = data;
-}
 Leaf::Leaf(Node* parent, std::uint8_t header, std::uint64_t data) : Node(parent, header) {
 	this->data = data;
 }
--- Leaf.h
+++ Leaf.h
@@ -2,8 +2,6 @@
 #include "Node.h"
 #include "BinaryReadWrite.h"
 class Leaf : public Node {
-private:
-	void _init(Node* parent, std::uint8_t header, std::uint64_t data);
 public:
 	std::uint64_t data;
 	Leaf(Node* parent, std::uint8_t header, std::uint64_t data);
