2f7831704dd7cd0a1bb81ea32eba956b9d6d40f8
BenMcLean <mclean.ben@gmail.com>
2024-06-24 12:33:50 -0500

    Leaf should use BinaryReadWrite.readU64

--- Leaf.cpp
+++ Leaf.cpp
@@ -7,7 +7,7 @@ Leaf::Leaf(Node* parent, std::uint8_t header, std::uint64_t data) : Node(parent,
 	this->data = data;
 }
 Leaf::Leaf(Node* parent, std::istream& in) : Node(parent, in) {
-	in >> data;
+	data = BinaryReadWrite::readU64(in);
 }
 void Leaf::write(std::ostream& out) const {
 	Node::write(out);
