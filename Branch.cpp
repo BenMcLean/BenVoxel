d15c2cbc0cf8dc870c5dfd6fd1559487e860318f
BenMcLean <mclean.ben@gmail.com>
2024-06-24 20:17:56 -0500

    oh, need to place children correctly by octant

--- Branch.cpp
+++ Branch.cpp
@@ -4,10 +4,12 @@ BenVoxel::Branch::Branch(Node* parent, std::uint8_t header, std::array<Node*, 8>
 }
 BenVoxel::Branch::Branch(Node* parent, std::istream& in) : Node(parent, in) {
 	std::uint8_t count = ((in.get() >> 3) & 7) + 1;
-	for (std::uint8_t i = 0; i < 8; i++)
-		children[i] = (in.peek() & 0x80) > 0 ?
-		(Node*)(new Leaf(this, in))
-		: (Node*)(new Branch(this, in));
+	for (std::uint8_t i = 0; i < 8; i++) {
+		std::uint8_t header = in.peek();
+		children[header & 7] = (header & 0x80) > 0 ?
+			(Node*)(new Leaf(this, in))
+			: (Node*)(new Branch(this, in));
+	}
 }
 void BenVoxel::Branch::write(std::ostream& out) const {
 	for (std::uint8_t i = 0; i < 8; i++)
