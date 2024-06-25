4119d004064f51c61d71d466ce184bce24e7b466
BenMcLean <mclean.ben@gmail.com>
2024-06-25 12:35:26 -0500

    Plugging a memory leak

--- Node.h
+++ Node.h
@@ -5,10 +5,11 @@ namespace BenVoxel {
 	class Node {
 	protected:
 		std::uint8_t octant;
-		Branch* parent;//Not owned
+		Branch* parent;//Not owned, nullptr indicates root Branch
 	public:
 		Node(Branch* parent, std::uint8_t header);
 		Node(Branch* parent, std::istream& in);
+		virtual ~Node() = default;
 		virtual void write(std::ostream& out) const = 0;
 		std::uint8_t getOctant() const;
 	};
