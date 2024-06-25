eb8fea4fb43e782c7c61c46a9fe756fb6e6ee3c7
BenMcLean <mclean.ben@gmail.com>
2024-06-24 21:37:31 -0500

    Branch should input the correct number of children

--- Branch.cpp
+++ Branch.cpp
@@ -4,7 +4,7 @@ BenVoxel::Branch::Branch(Node* parent, std::uint8_t header, std::array<Node*, 8>
 }
 BenVoxel::Branch::Branch(Node* parent, std::istream& in) : Node(parent, in), children{} {
 	std::uint8_t count = ((in.get() >> 3) & 7) + 1;
-	for (std::uint8_t i = 0; i < 8; i++) {
+	for (std::uint8_t i = 0; i < count; i++) {
 		std::uint8_t header = in.peek();
 		children[header & 7] = (header & 0x80) > 0 ?
 			(Node*)(new Leaf(this, in))
