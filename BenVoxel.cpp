5604f2d4c0b550bd90adf5dc36836383367dabb5
BenMcLean <mclean.ben@gmail.com>
2024-06-26 17:04:32 -0500

    these are unnecessary it seems

--- BenVoxel.cpp
+++ BenVoxel.cpp
@@ -3,12 +3,10 @@
 int main()
 {
 	std::ifstream inFile;
-	inFile >> std::noskipws;
 	inFile.open("EMPTY.BEN", std::ios::binary);
 	BenVoxel::SparseVoxelOctree svo = BenVoxel::SparseVoxelOctree(inFile);
 	inFile.close();
 	std::ofstream outFile;
-	outFile << std::noskipws;
 	outFile.open("EMPTY2.BEN", std::ios::binary);
 	svo.write(outFile);
 	outFile.close();
