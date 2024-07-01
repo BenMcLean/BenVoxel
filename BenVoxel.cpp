398071a83dcac0ca0b7b7d4323dbc97fd34c8d55
BenMcLean <mclean.ben@gmail.com>
2024-07-01 08:40:16 -0500

    Trying to test copying all the voxels

--- BenVoxel.cpp
+++ BenVoxel.cpp
@@ -3,12 +3,19 @@
 int main()
 {
 	std::ifstream inFile;
-	inFile.open("SORA.BEN", std::ios::binary);
+	inFile.open("..\\..\\..\\SORA.BEN", std::ios::binary);
 	BenVoxel::SparseVoxelOctree svo = BenVoxel::SparseVoxelOctree(inFile);
 	inFile.close();
 	std::ofstream outFile;
 	outFile.open("SORA2.BEN", std::ios::binary);
-	svo.write(outFile);
+	BenVoxel::SparseVoxelOctree(svo.voxels()).write(outFile);
 	outFile.close();
+
+	//BenVoxel::SparseVoxelOctree svo = {};
+	//svo.set(1, 1, 1, 1);
+	//std::ofstream outFile;
+	//outFile.open("OUT.BEN", std::ios::binary);
+	//svo.write(outFile);
+	//outFile.close();
 	return 0;
 }
